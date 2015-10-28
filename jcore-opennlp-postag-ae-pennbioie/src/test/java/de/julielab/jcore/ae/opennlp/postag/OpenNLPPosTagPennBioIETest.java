226fef72f61a46d79bcec874137b53b70795b03b
khituras <chew@gmx.net>
2015-10-28 16:57:00 +0100

    test class

--- jcore-opennlp-postag-ae-pennbioie/src/test/java/de/julielab/jcore/ae/opennlp/postag/OpenNLPPosTagPennBioIETest.java
+++ jcore-opennlp-postag-ae-pennbioie/src/test/java/de/julielab/jcore/ae/opennlp/postag/OpenNLPPosTagPennBioIETest.java
@@ -1,7 +1,6 @@
 package de.julielab.jcore.ae.opennlp.postag;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.cas.FSIterator;
@@ -18,13 +17,16 @@ public class OpenNLPPosTagPennBioIETest {
 	@Test
 	public void testPackage() throws Exception {
 		JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types");
-		jCas.setDocumentText("The gene was regulated.");
-		new Sentence(jCas, 0, 23).addToIndexes();
+		jCas.setDocumentText("The gene was regulated in the cell.");
+		new Sentence(jCas, 0, 35).addToIndexes();
 		new Token(jCas, 0, 3).addToIndexes();
 		new Token(jCas, 4, 8).addToIndexes();
 		new Token(jCas, 9, 12).addToIndexes();
 		new Token(jCas, 13, 22).addToIndexes();
-		new Token(jCas, 22, 23).addToIndexes();
+		new Token(jCas, 23, 25).addToIndexes();
+		new Token(jCas, 26, 29).addToIndexes();
+		new Token(jCas, 30, 34).addToIndexes();
+		new Token(jCas, 34, 35).addToIndexes();
 		AnalysisEngine ae = AnalysisEngineFactory.createEngine("de.julielab.jcore.ae.opennlp.postag.desc.jcore-opennlp-postag-ae-pennbioie");
 		ae.process(jCas);
 		FSIterator<Annotation> it = jCas.getAnnotationIndex(Token.type).iterator();
@@ -40,6 +42,15 @@ public class OpenNLPPosTagPennBioIETest {
 		t = (Token) it.next();
 		assertNotNull(t.getPosTag());
 		assertEquals("VBN", t.getPosTag(0).getValue());
+		t = (Token) it.next();
+		assertNotNull(t.getPosTag());
+		assertEquals("IN", t.getPosTag(0).getValue());
+		t = (Token) it.next();
+		assertNotNull(t.getPosTag());
+		assertEquals("DT", t.getPosTag(0).getValue());
+		t = (Token) it.next();
+		assertNotNull(t.getPosTag());
+		assertEquals("NN", t.getPosTag(0).getValue());
 		t = (Token) it.next();
 		assertNotNull(t.getPosTag());
 		assertEquals(".", t.getPosTag(0).getValue());
