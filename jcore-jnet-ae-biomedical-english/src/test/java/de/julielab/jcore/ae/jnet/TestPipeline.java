6ccf920e90bf0a232cdbf78b98b969003a3d7da8
Franz Matthies <franz.matthies@uni-jena.de>
2015-11-13 14:19:53 +0100

    small changes to test

--- jcore-jnet-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/jnet/TestPipeline.java
+++ jcore-jnet-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/jnet/TestPipeline.java
@@ -1,43 +1,47 @@
 package de.julielab.jcore.ae.jnet;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.resource.ResourceSpecifier;
 import org.apache.uima.util.XMLInputSource;
-import static org.junit.Assert.*;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestPipeline {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(TestPipeline.class);
-	private static final File ENTITY_ANNOTATOR_DESC = new File("src/main/resources/de/julielab/jcore/ae/jnet/desc/jcore-jnet-ae-biomedical-english.xml");
-	/**
-	 * test whether Annotator can be initialized properly from given descriptor
-	 */
-	public void testInitialize() {
-		LOGGER.debug("testInitialize()");
-		AnalysisEngine entityAnnotator = null;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestPipeline.class);
+    private static final File ENTITY_ANNOTATOR_DESC = new File(
+            "src/main/resources/de/julielab/jcore/ae/jnet/desc/jcore-jnet-ae-biomedical-english.xml");
 
-		XMLInputSource taggerXML = null;
-		ResourceSpecifier taggerSpec = null;
+    /**
+     * test whether Annotator can be initialized properly from given descriptor
+     */
+    @Test
+    public void testInitialize() {
+        LOGGER.debug("testInitialize()");
+        AnalysisEngine entityAnnotator = null;
 
-		try {
-			taggerXML = new XMLInputSource(ENTITY_ANNOTATOR_DESC);
-			taggerSpec = UIMAFramework.getXMLParser().parseResourceSpecifier(
-					taggerXML);
-			entityAnnotator = UIMAFramework.produceAnalysisEngine(taggerSpec);
-		} catch (final Exception e) {
-			LOGGER.error("testInitialize()", e);
-		}
+        XMLInputSource taggerXML = null;
+        ResourceSpecifier taggerSpec = null;
 
-		if (entityAnnotator != null)
-			assertTrue(true);
-		else
-			assertTrue(false);
+        try {
+            taggerXML = new XMLInputSource(ENTITY_ANNOTATOR_DESC);
+            taggerSpec = UIMAFramework.getXMLParser().parseResourceSpecifier(taggerXML);
+            entityAnnotator = UIMAFramework.produceAnalysisEngine(taggerSpec);
+        } catch (final Exception e) {
+            LOGGER.error("testInitialize()", e);
+        }
 
-	}
+        if (entityAnnotator != null) {
+            assertTrue(true);
+        } else {
+            assertTrue(false);
+        }
+
+    }
 }
