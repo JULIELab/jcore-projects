20aab5a7b19c3d0c30c89f5dcf251235f6e6a5a7
Franz Matthies <franz.matthies@uni-jena.de>
2015-11-13 14:12:38 +0100

    added basic tests for model loading and descriptor init

---
+++ jcore-jsbd-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/jsbd/TestPipeline.java
@@ -0,0 +1,47 @@
+package de.julielab.jcore.ae.jsbd;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.resource.ResourceSpecifier;
+import org.apache.uima.util.XMLInputSource;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestPipeline {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestPipeline.class);
+    private static final File ENTITY_ANNOTATOR_DESC = new File(
+            "src/main/resources/de/julielab/jcore/ae/jsbd/desc/jcore-jsbd-biomedical-english.xml");
+
+    /**
+     * test whether Annotator can be initialized properly from given descriptor
+     */
+    @Test
+    public void testInitialize() {
+        LOGGER.debug("testInitialize()");
+        AnalysisEngine entityAnnotator = null;
+
+        XMLInputSource taggerXML = null;
+        ResourceSpecifier taggerSpec = null;
+
+        try {
+            taggerXML = new XMLInputSource(ENTITY_ANNOTATOR_DESC);
+            taggerSpec = UIMAFramework.getXMLParser().parseResourceSpecifier(taggerXML);
+            entityAnnotator = UIMAFramework.produceAnalysisEngine(taggerSpec);
+        } catch (final Exception e) {
+            LOGGER.error("testInitialize()", e);
+        }
+
+        if (entityAnnotator != null) {
+            assertTrue(true);
+        } else {
+            assertTrue(false);
+        }
+
+    }
+}
