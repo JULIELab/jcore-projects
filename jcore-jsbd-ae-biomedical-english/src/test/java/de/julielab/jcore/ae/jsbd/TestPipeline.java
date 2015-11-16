bbc1dc4e52b60c4ecf210a4a1ec699262ecc7cb7
Franz Matthies <franz.matthies@uni-jena.de>
2015-11-16 16:18:16 +0100

    added another test

--- jcore-jsbd-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/jsbd/TestPipeline.java
+++ jcore-jsbd-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/jsbd/TestPipeline.java
@@ -6,11 +6,18 @@ import java.io.File;
 
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceSpecifier;
 import org.apache.uima.util.XMLInputSource;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import de.julielab.jcore.types.Sentence;
 
 public class TestPipeline {
 
@@ -18,16 +25,26 @@ public class TestPipeline {
     private static final File ENTITY_ANNOTATOR_DESC = new File(
             "src/main/resources/de/julielab/jcore/ae/jsbd/desc/jcore-jsbd-biomedical-english.xml");
 
+    private static final String testText = "Down-regulation of protein kinase C: a potential mechanism for 2-amino-3-methylimidazo[4,5-f]quinoline-mediated immunosuppression."
+            + " Immunosuppressive mechanism of food-born mutagenic and carcinogenic heterocyclic amine, 2-amino-3-methylimidazo[4,5-f]quinoline (IQ), was studied in murine spleen cells."
+            + " IQ suppressed the IL-2 secretion and its mRNA expression."
+            + " Treatment of IQ also decreased PKC activity in both the membrane fraction and the cytosol fraction."
+            + " Treatment of PMA resulted in the recovery of IQ-induced suppression of IL-2 production, but the addition of ionomycin (Io) had no effect."
+            + " Subsequently, we examined the effect of IQ on the activation of NF-kappaB, AP-1, and NF-AT, which are key transcription factors for IL-2 transcription."
+            + " The activation of NF-kappaB and AP-1 was markedly inhibited by IQ in PMA/PHA-stimulated cells."
+            + " Meanwhile, NF-AT was not affected by IQ in PMA/Io-stimulated cells."
+            + " These results suggest that the immunosuppression induced by IQ might be associated with the down-regulation of PKC and subsequent blockade of the activation of NF-kappaB and AP-1 in the IL-2 gene expression.";
+
+    private XMLInputSource taggerXML = null;
+    private ResourceSpecifier taggerSpec = null;
+    private AnalysisEngine entityAnnotator = null;
+
     /**
      * test whether Annotator can be initialized properly from given descriptor
      */
-    @Test
+    @Before
     public void testInitialize() {
-        LOGGER.debug("testInitialize()");
-        AnalysisEngine entityAnnotator = null;
-
-        XMLInputSource taggerXML = null;
-        ResourceSpecifier taggerSpec = null;
+        LOGGER.info("testInitialize()");
 
         try {
             taggerXML = new XMLInputSource(ENTITY_ANNOTATOR_DESC);
@@ -43,5 +60,24 @@ public class TestPipeline {
             assertTrue(false);
         }
 
+    }
+
+    /**
+     * test whether Annotator splits sentences correctly
+     */
+    @Test
+    public void testSplitting() throws Exception {
+        LOGGER.info("testSplitting()");
+        JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-all-types");
+        jCas.setDocumentText(testText);
+        entityAnnotator.process(jCas);
+
+        FSIterator<Annotation> sents = jCas.getAnnotationIndex(Sentence.type).iterator();
+        int sCount = 0;
+        while (sents.hasNext()) {
+            Sentence sent = (Sentence) sents.next();
+            sCount += 1;
+        }
+        assertTrue(sCount == 9);
     }
 }
