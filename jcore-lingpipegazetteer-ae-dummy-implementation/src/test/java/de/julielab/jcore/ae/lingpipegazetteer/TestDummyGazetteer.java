634473d549ee286c3c30b061d6a9595145d4404b
Franz Matthies <franz.matthies@uni-jena.de>
2015-11-17 12:34:41 +0100

    added test

---
+++ jcore-lingpipegazetteer-ae-dummy-implementation/src/test/java/de/julielab/jcore/ae/lingpipegazetteer/TestDummyGazetteer.java
@@ -0,0 +1,16 @@
+package de.julielab.jcore.ae.lingpipegazetteer;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+public class TestDummyGazetteer {
+    @Test
+    public void testAE() throws Exception {
+        JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-all-types");
+        AnalysisEngine engine = AnalysisEngineFactory
+                .createEngine("de.julielab.jcore.ae.lingpipegazetteer.dummy.desc.ApproxGazetteerAnnotatorTest");
+    }
+}
