562d90de9a546b54f30ee6b665ac422076985b53
Franz Matthies <franz.matthies@uni-jena.de>
2015-11-19 10:31:36 +0100

    updated readme and tests

---
+++ jcore-mstparser-ae-biomedical-english/README.md
@@ -0,0 +1,13 @@
+# MST Parser Genia 2008
+
+### Evaluation
+10-fold crossvalidation on the Genia corpus results in the following:
+```
+EVALUATION PERFORMANCE:
+Tokens: 489905
+Correct: 438897
+Unlabeled Accuracy: 0.8958818546452884
+Unlabeled Complete Correct: 0.27622964125317895
+Labeled Accuracy: 0.8839693409946826
+Labeled Complete Correct: 0.23018234943996538
+```
---
+++ jcore-mstparser-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/mstparser/TestModel.java
@@ -0,0 +1,16 @@
+package de.julielab.jcore.ae.mstparser;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+public class TestModel {
+    @Test
+    public void testAE() throws Exception {
+        JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-all-types");
+        AnalysisEngine engine = AnalysisEngineFactory
+                .createEngine("de.julielab.jcore.ae.mstparser.desc.jcore-mstparser-biomedical-english");
+    }
+}
