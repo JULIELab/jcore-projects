675b915e5f1ce09efc08d044b4def85eeb325b15
Franz Matthies <franz.matthies@uni-jena.de>
2015-11-19 10:48:19 +0100

    started modifying test to be more verbose
    test fails but I can't work out why as it gives me no pointer what went
    wrong...

--- jcore-pubmed-reader/src/test/java/de/julielab/jcore/reader/xml/PubmedReaderTest.java
+++ jcore-pubmed-reader/src/test/java/de/julielab/jcore/reader/xml/PubmedReaderTest.java
@@ -3,7 +3,6 @@ package de.julielab.jcore.reader.xml;
 import static org.junit.Assert.*;
 
 import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
@@ -20,78 +19,91 @@ import de.julielab.jcore.types.pubmed.Header;
 import de.julielab.jcore.types.pubmed.ManualDescriptor;
 
 public class PubmedReaderTest {
-	@Test
-	public void testMedlineReader() throws Exception {
-		JCas jCas = JCasFactory
-				.createJCas("de.julielab.jcore.types.jcore-document-meta-pubmed-types", "de.julielab.jcore.types.jcore-document-structure-types");
-		CollectionReaderDescription medlineReaderDescription = CollectionReaderFactory
-				.createReaderDescriptionFromPath("src/main/resources/de/julielab/jcore/reader/xml/desc/jcore-pubmed-reader.xml");
-		CollectionReader medlineReader = CollectionReaderFactory.createReader(medlineReaderDescription, XMLReader.PARAM_INPUT_FILE,
-				"src/test/resources/pubmedDocs/7723770.xml");
-		assertTrue(medlineReader.hasNext());
-		medlineReader.getNext(jCas.getCas());
+    @Test
+    public void testPubmedReader() throws Exception {
+        JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-document-meta-pubmed-types",
+                "de.julielab.jcore.types.jcore-document-structure-types");
+        CollectionReaderDescription pubmedReaderDescription = CollectionReaderFactory.createReaderDescriptionFromPath(
+                "src/main/resources/de/julielab/jcore/reader/xml/desc/jcore-pubmed-reader.xml");
+        CollectionReader pubmedReader = CollectionReaderFactory.createReader(pubmedReaderDescription,
+                XMLReader.PARAM_INPUT_FILE, "src/test/resources/pubmedDocs/7723770.xml");
+        assertTrue("Assertion failure", pubmedReader.hasNext());
+        pubmedReader.getNext(jCas.getCas());
 
-		assertEquals("7723770", JCasUtil.selectSingle(jCas, Header.class).getDocId());
-		assertNotNull(jCas.getDocumentText());
-		assertTrue(jCas.getDocumentText().length() > 0);
-	}
+        assertEquals("Assertion failure", "7723770", JCasUtil.selectSingle(jCas, Header.class).getDocId());
+        assertNotNull("Assertion failure", jCas.getDocumentText());
+        assertTrue("Assertion failure", jCas.getDocumentText().length() > 0);
+    }
 
-	@Test
-	public void testKeywords() throws Exception {
-		JCas jCas = JCasFactory
-				.createJCas("de.julielab.jcore.types.jcore-document-meta-pubmed-types", "de.julielab.jcore.types.jcore-document-structure-types");
-		CollectionReaderDescription medlineReaderDescription = CollectionReaderFactory
-				.createReaderDescriptionFromPath("src/main/resources/de/julielab/jcore/reader/xml/desc/jcore-pubmed-reader.xml");
-		CollectionReader medlineReader = CollectionReaderFactory.createReader(medlineReaderDescription, XMLReader.PARAM_INPUT_FILE,
-				"src/test/resources/pubmedDocs/26504747.xml");
-		assertTrue(medlineReader.hasNext());
-		medlineReader.getNext(jCas.getCas());
+    @Test
+    public void testKeywords() throws Exception {
+        JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-document-meta-pubmed-types",
+                "de.julielab.jcore.types.jcore-document-structure-types");
+        CollectionReaderDescription medlineReaderDescription = CollectionReaderFactory.createReaderDescriptionFromPath(
+                "src/main/resources/de/julielab/jcore/reader/xml/desc/jcore-pubmed-reader.xml");
+        CollectionReader medlineReader = CollectionReaderFactory.createReader(medlineReaderDescription,
+                XMLReader.PARAM_INPUT_FILE, "src/test/resources/pubmedDocs/26504747.xml");
+        assertTrue(medlineReader.hasNext());
+        medlineReader.getNext(jCas.getCas());
 
-		assertEquals("26504747", JCasUtil.selectSingle(jCas, Header.class).getDocId());
-		assertNotNull(jCas.getDocumentText());
-		assertTrue(jCas.getDocumentText().length() > 0);
+        assertEquals("26504747", JCasUtil.selectSingle(jCas, Header.class).getDocId());
+        assertNotNull(jCas.getDocumentText());
+        assertTrue(jCas.getDocumentText().length() > 0);
 
-		FSIterator<Annotation> it = jCas.getAnnotationIndex(ManualDescriptor.type).iterator();
-		assertTrue(it.hasNext());
-		ManualDescriptor md = (ManualDescriptor) it.next();
-		FSArray kwl = md.getKeywordList();
-		Keyword kw = (Keyword) kwl.get(0);
-		assertEquals("MTOR", kw.getName());
-		kw = (Keyword) kwl.get(1);
-		assertEquals("Oncogene", kw.getName());
-		kw = (Keyword) kwl.get(2);
-		assertEquals("PI3K/Akt signaling", kw.getName());
-		kw = (Keyword) kwl.get(3);
-		assertEquals("Thyroid", kw.getName());
-		kw = (Keyword) kwl.get(4);
-		assertEquals("mTOR Mutation", kw.getName());
-	}
-	
-	@Test
-	public void testAffiliations() throws Exception {
-		JCas jCas = JCasFactory
-				.createJCas("de.julielab.jcore.types.jcore-document-meta-pubmed-types", "de.julielab.jcore.types.jcore-document-structure-types");
-		CollectionReaderDescription medlineReaderDescription = CollectionReaderFactory
-				.createReaderDescriptionFromPath("src/main/resources/de/julielab/jcore/reader/xml/desc/jcore-pubmed-reader.xml");
-		CollectionReader medlineReader = CollectionReaderFactory.createReader(medlineReaderDescription, XMLReader.PARAM_INPUT_FILE,
-				"src/test/resources/pubmedDocs/26504747.xml");
-		assertTrue(medlineReader.hasNext());
-		medlineReader.getNext(jCas.getCas());
+        FSIterator<Annotation> it = jCas.getAnnotationIndex(ManualDescriptor.type).iterator();
+        assertTrue(it.hasNext());
+        ManualDescriptor md = (ManualDescriptor) it.next();
+        FSArray kwl = md.getKeywordList();
+        Keyword kw = (Keyword) kwl.get(0);
+        assertEquals("MTOR", kw.getName());
+        kw = (Keyword) kwl.get(1);
+        assertEquals("Oncogene", kw.getName());
+        kw = (Keyword) kwl.get(2);
+        assertEquals("PI3K/Akt signaling", kw.getName());
+        kw = (Keyword) kwl.get(3);
+        assertEquals("Thyroid", kw.getName());
+        kw = (Keyword) kwl.get(4);
+        assertEquals("mTOR Mutation", kw.getName());
+    }
 
-		Header h = JCasUtil.selectSingle(jCas, Header.class);
-		AuthorInfo ai = h.getAuthors(0);
-		assertEquals("Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-		ai = h.getAuthors(1);
-		assertEquals("Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia ; Department of Zoology, King Saud University, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-		ai = h.getAuthors(2);
-		assertEquals("Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-		ai = h.getAuthors(3);
-		assertEquals("Department of Pathology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-		ai = h.getAuthors(4);
-		assertEquals("Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-		ai = h.getAuthors(5);
-		assertEquals("Department of Zoology, King Saud University, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-		ai = h.getAuthors(6);
-		assertEquals("Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.", ai.getAffiliation());
-	}
+    @Test
+    public void testAffiliations() throws Exception {
+        JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-document-meta-pubmed-types",
+                "de.julielab.jcore.types.jcore-document-structure-types");
+        CollectionReaderDescription medlineReaderDescription = CollectionReaderFactory.createReaderDescriptionFromPath(
+                "src/main/resources/de/julielab/jcore/reader/xml/desc/jcore-pubmed-reader.xml");
+        CollectionReader medlineReader = CollectionReaderFactory.createReader(medlineReaderDescription,
+                XMLReader.PARAM_INPUT_FILE, "src/test/resources/pubmedDocs/26504747.xml");
+        assertTrue(medlineReader.hasNext());
+        medlineReader.getNext(jCas.getCas());
+
+        Header h = JCasUtil.selectSingle(jCas, Header.class);
+        AuthorInfo ai = h.getAuthors(0);
+        assertEquals(
+                "Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+        ai = h.getAuthors(1);
+        assertEquals(
+                "Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia ; Department of Zoology, King Saud University, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+        ai = h.getAuthors(2);
+        assertEquals(
+                "Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+        ai = h.getAuthors(3);
+        assertEquals(
+                "Department of Pathology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+        ai = h.getAuthors(4);
+        assertEquals(
+                "Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+        ai = h.getAuthors(5);
+        assertEquals("Department of Zoology, King Saud University, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+        ai = h.getAuthors(6);
+        assertEquals(
+                "Molecular Endocrinology Section, Department of Molecular Oncology, King Faisal Specialist Hospital and Research Center, Riyadh, Kingdom of Saudi Arabia.",
+                ai.getAffiliation());
+    }
 }
