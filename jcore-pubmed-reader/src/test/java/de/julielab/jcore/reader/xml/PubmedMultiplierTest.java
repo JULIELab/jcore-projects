62e6de2429bc0c425566f6fdf2719e102a95c28b
khituras <chew@gmx.net>
2018-04-05 13:52:57 +0200

    Minor change.

--- jcore-pubmed-reader/src/test/java/de/julielab/jcore/reader/xml/PubmedMultiplierTest.java
+++ jcore-pubmed-reader/src/test/java/de/julielab/jcore/reader/xml/PubmedMultiplierTest.java
@@ -5,8 +5,14 @@ import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CollectionProcessingEngine;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.collection.EntityProcessStatus;
+import org.apache.uima.collection.StatusCallbackListener;
 import org.apache.uima.collection.metadata.CpeDescriptorException;
+import org.apache.uima.fit.cpe.CpeBuilder;
+import org.apache.uima.fit.cpe.CpePipeline;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
@@ -19,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.junit.Assert.*;
 
 /**
@@ -41,7 +48,9 @@ public class PubmedMultiplierTest {
         AnalysisEngineDescription testAe = AnalysisEngineFactory.createEngineDescription(TestAe.class);
         AnalysisEngineDescription topAAE = AnalysisEngineFactory.createEngineDescription(Arrays.asList(multiplier, testAe), Arrays.asList("Multiplier", "TestAe"), null, null, null);
 
-        SimplePipeline.runPipeline(reader, topAAE);
+        // We must wrap the CM with its substream components into an AAE of itself. The CPE directly doesn't know how
+        // to deal with the multiplier.
+        CpePipeline.runPipeline(reader, topAAE);
 
         // The seenPmids list is filled by the TestAe (see code below).
         assertEquals(177, seenPmids.size());
@@ -59,6 +68,55 @@ public class PubmedMultiplierTest {
             // Write PMIDs into the list. The samples often don't have
             // abstracts or even titles.
             seenPmids.add(docId);
+        }
+    }
+
+    private static class StatusCallbackListenerImpl implements StatusCallbackListener {
+
+        private final List<Exception> exceptions = new ArrayList<Exception>();
+
+        private boolean isProcessing = true;
+
+        public void entityProcessComplete(CAS arg0, EntityProcessStatus arg1) {
+            if (arg1.isException()) {
+                for (Exception e : arg1.getExceptions()) {
+                    exceptions.add(e);
+                }
+            }
+        }
+
+        public void aborted() {
+            synchronized (this) {
+                if (isProcessing) {
+                    isProcessing = false;
+                    notify();
+                }
+            }
+        }
+
+        public void batchProcessComplete() {
+            // Do nothing
+        }
+
+        public void collectionProcessComplete() {
+            synchronized (this) {
+                if (isProcessing) {
+                    isProcessing = false;
+                    notify();
+                }
+            }
+        }
+
+        public void initializationComplete() {
+            // Do nothing
+        }
+
+        public void paused() {
+            // Do nothing
+        }
+
+        public void resumed() {
+            // Do nothing
         }
     }
 }
