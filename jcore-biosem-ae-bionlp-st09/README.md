6ad29dad2e9a9ee80988c59a8087ffdec946ab20
Franz Matthies <franz.matthies@uni-jena.de>
2018-08-22 10:56:06 +0200

    added bionlp-st09 & -st13 models for biosem-ae

---
+++ jcore-biosem-ae-bionlp-st09/README.md
@@ -0,0 +1,14 @@
+# BioNLP Shared Task 09 Model  
+
+**Descriptor Path**:
+```
+de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st09
+```
+
+This pre-built project's model was trained on the train set of the [BioNLP ST'09 resources](http://www.nactem.ac.uk/tsujii/GENIA/SharedTask/).
+CAS' that feature annotations for at least genes (`de.julielab.jcore.types.Gene`) can be processed and will be further annotated with:
+```
+de.julielab.jcore.types.ArgumentMention
+de.julielab.jcore.types.EventMention
+de.julielab.jcore.types.EventTrigger
+```
---
+++ jcore-biosem-ae-bionlp-st13/src/test/java/de/julielab/jcore/ae/biosem/bionlpst13/TestDifficultDocument.java
@@ -0,0 +1,106 @@
+package de.julielab.jcore.ae.biosem.bionlpst13;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.impl.XmiCasDeserializer;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.junit.Test;
+
+import de.julielab.jcore.types.EventMention;
+
+/**
+ * With 'difficult document' we mean a document that used to make problems with BioSem
+ */
+public class TestDifficultDocument {
+	@Test
+	public void testDifficultDoc1() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/11250876.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+	@Test
+	public void testDifficultDoc2() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/10806352.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+	@Test
+	public void testDifficultDoc3() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/20935495.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+	@Test
+	public void testDifficultDoc4() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/21681342.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+	@Test
+	public void testDifficultDoc5() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/22675210.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+	@Test
+	public void testDifficultDoc6() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/23380144.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+	@Test
+	public void testDifficultDoc7() throws Exception {
+		JCas jCas = JCasFactory
+				.createJCas("de.julielab.jcore.types.jcore-all-types");
+		XmiCasDeserializer.deserialize(new FileInputStream("src/test/resources/2806624.xmi"), jCas.getCas());
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createEngine("de.julielab.jcore.ae.biosem.desc.jcore-biosem-ae-bionlp-st13");
+		engine.process(jCas);
+		FSIterator<Annotation> it = jCas.getAnnotationIndex(EventMention.type).iterator();
+		assertTrue(it.hasNext());
+	}
+	
+}
