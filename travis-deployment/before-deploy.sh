b0005f7985255e19123723d09fc434d81db7b1d4
khituras <chew@gmx.net>
2018-12-28 21:39:21 +0100

    Setting up deployment from Travis

---
+++ travis-deployment/before-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_8ca4d2f260b1_key -iv $encrypted_8ca4d2f260b1_iv -in travis-deployment/codesigning.asc.enc -out travis-deployment/codesigning.asc -d
+    if [ ! "$?" -eq "0" ]; then
+        echo "Could not decrypt gpg key";
+        exit 1;
+    fi
+	gpg --fast-import travis-deployment/codesigning.asc
+	if [ ! "$?" -eq "0" ]; then
+            echo "Could not import GPG signing key";
+            exit 2;
+        fi
+	gpg -k
+	gpg -K
+else
+    echo "WARNING: The conditions for gpg key import were not met, no gpg signing key will be available for deploy."
+fi
+
