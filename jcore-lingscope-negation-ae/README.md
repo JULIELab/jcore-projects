9a89717962003a2868121452b33701dec7fcf06d
khituras <chew@gmx.net>
2019-01-14 09:21:07 +0100

    Added a Lingscope AE for negation cue and scope detection.
    It detects negation cue words with the baseline model which uses
    a dictionary. It detects the scopes of the negations using a CRF
    model that does not replace the cue word with the "CUE" string.
    The Respective paper made these recommendations when you are
    primarily interested into the scope of the negation. The baseline
    cue model has a better coverage than the CRF cue detection model and
    the replacement of the cue word with "CUE" did rather hurt then
    help performance as reported by the paper(https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3000754/).

---
+++ jcore-lingscope-negation-ae/README.md
@@ -0,0 +1,34 @@
+# JCoRe Lingscope Negation AE
+
+**Descriptor Path**:
+```
+de.julielab.desc.jcore-lingscope-negation-ae
+```
+
+This component uses Lingscope with the baseline negation cue tagger and a CRF scope detector.
+
+
+
+**1. Parameters**
+
+| Parameter Name | Parameter Type | Mandatory | Multivalued | Description |
+|----------------|----------------|-----------|-------------|-------------|
+| param1 | UIMA-Type | Boolean | Boolean | Description |
+| param2 | UIMA-Type | Boolean | Boolean | Description |
+
+**2. Predefined Settings**
+
+| Parameter Name | Parameter Syntax | Example |
+|----------------|------------------|---------|
+| param1 | Syntax-Description | `Example` |
+| param2 | Syntax-Description | `Example` |
+
+**3. Capabilities**
+
+| Type | Input | Output |
+|------|:-----:|:------:|
+| de.julielab.jcore.types.TYPE |  | `+` |
+| de.julielab.jcore.types.ace.TYPE | `+` |  |
+
+
+[1] Some Literature?
---
+++ jcore-lingscope-negation-ae/src/test/java/de/julielab/jcore/ae/lingscope/LingscopeNegationAeTest.java
@@ -0,0 +1,56 @@
+package de.julielab.jcore.ae.lingscope;
+
+import de.julielab.jcore.types.*;
+import de.julielab.jcore.utility.JCoReTools;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+public class LingscopeNegationAeTest {
+    @Test
+    public void testWithoutLikelihoodDict() throws Exception {
+        final JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types", "de.julielab.jcore.types.jcore-semantics-mention-types");
+        final AnalysisEngine engine = AnalysisEngineFactory.createEngine("de.julielab.jcore.ae.lingscope.desc.jcore-lingscope-negation-ae");
+        jCas.setDocumentText("The patient denied leg pain but complained about a headache.");
+        new Sentence(jCas, 0, jCas.getDocumentText().length()).addToIndexes();
+        addToken(jCas, 0, 3, "DT", "The");
+        addToken(jCas, 4, 11, "NN", "patient");
+        addToken(jCas, 12, 18, "VBD", "deny");
+        addToken(jCas, 19, 22, "NN", "leg");
+        addToken(jCas, 23, 27, "NN", "pain");
+        addToken(jCas, 28, 31, "CC", "but");
+        addToken(jCas, 32, 42, "VBD", "complain");
+        addToken(jCas, 43, 48, "IN", "about");
+        addToken(jCas, 49, 50, "DT", "a");
+        addToken(jCas, 51, 59, "NN", "headache");
+        addToken(jCas, 59, 60, ".", ".");
+
+        engine.process(jCas);
+
+        final LikelihoodIndicator indicator = JCasUtil.selectSingle(jCas, LikelihoodIndicator.class);
+        assertThat(indicator.getBegin()).isEqualTo(12);
+        assertThat(indicator.getEnd()).isEqualTo(18);
+        assertThat(indicator.getLikelihood()).isEqualTo("negation");
+
+        final Scope scope = JCasUtil.selectSingle(jCas, Scope.class);
+        assertThat(scope.getCue() == indicator);
+        assertThat(scope.getBegin()).isEqualTo(12);
+        // That the scope is this long is actually an error of the CRF
+        assertThat(scope.getEnd()).isEqualTo(59);
+
+    }
+
+    private void addToken(JCas jCas, int begin, int end, String posTag, String lemma) {
+        final Token token = new Token(jCas, begin, end);
+        final PennBioIEPOSTag p1 = new PennBioIEPOSTag(jCas, begin, end);
+        p1.setValue(posTag);
+        token.setPosTag(JCoReTools.addToFSArray(null, p1));
+        final Lemma lemmaAnnotation = new Lemma(jCas, begin, end);
+        lemmaAnnotation.setValue(lemma);
+        token.setLemma(lemmaAnnotation);
+        token.addToIndexes();
+    }
+}
