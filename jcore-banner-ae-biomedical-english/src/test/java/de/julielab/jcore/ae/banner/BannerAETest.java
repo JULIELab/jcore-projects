5d0693f34fe45a327d27941979390cad371d6660
khituras <chew@gmx.net>
2021-06-18 11:33:17 +0200

    Revert "More jUnit fixing."
    This reverts commit 98cf047b0590f97222fe651db84fb8e6fe18ebbe.

--- jcore-banner-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/banner/BannerAETest.java
+++ jcore-banner-ae-biomedical-english/src/test/java/de/julielab/jcore/ae/banner/BannerAETest.java
@@ -8,6 +8,8 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,35 +17,36 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BannerAETest {
-    @Test
-    public void testDescriptor() throws Exception {
-        // just tag a single sentence with a test model that actually used that sentence as training data.
-        JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
-                "de.julielab.jcore.types.jcore-document-meta-types",
-                "de.julielab.jcore.types.jcore-semantics-biology-types");
-        // this is sentence P00055040A0000 from the test BC2GM train data in the jcore-banner-ae project
-        jcas.setDocumentText(
-                "Ten out-patients with pustulosis palmaris et plantaris were examined with direct immunofluorescence (IF) technique for deposition of fibrinogen, fibrin or its degradation products (FR-antigen) in affected and unaffected skin, together with heparin-precipitable fraction (HPF), cryoglobulin and total plasma fibrinogen in the blood.");
-        new Sentence(jcas, 0, jcas.getDocumentText().length()).addToIndexes();
+    private final static Logger log = LoggerFactory.getLogger(BannerAETest.class);
+	@Test
+	public void testDescriptor() throws Exception {
+		// just tag a single sentence with a test model that actually used that sentence as training data.
+		JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
+				"de.julielab.jcore.types.jcore-document-meta-types",
+				"de.julielab.jcore.types.jcore-semantics-biology-types");
+		// this is sentence P00055040A0000 from the test BC2GM train data in the jcore-banner-ae project
+		jcas.setDocumentText(
+				"Ten out-patients with pustulosis palmaris et plantaris were examined with direct immunofluorescence (IF) technique for deposition of fibrinogen, fibrin or its degradation products (FR-antigen) in affected and unaffected skin, together with heparin-precipitable fraction (HPF), cryoglobulin and total plasma fibrinogen in the blood.");
+		new Sentence(jcas, 0, jcas.getDocumentText().length()).addToIndexes();
 
         AnalysisEngine bannerAe = null;
-        bannerAe = AnalysisEngineFactory.createEngine("de.julielab.jcore.ae.banner.desc.jcore-banner-ae-biomedical-english");
-        bannerAe.process(jcas);
+                bannerAe= AnalysisEngineFactory.createEngine("de.julielab.jcore.ae.banner.desc.jcore-banner-ae-biomedical-english");
+		bannerAe.process(jcas);
 
-        // expected result from the GENE.eval.small file:
-        // P00055040A0000|116 125|fibrinogen
-        // P00055040A0000|127 132|fibrin
-        // P00055040A0000|158 167|FR-antigen
-        // P00055040A0000|243 254|cryoglobulin
-        // P00055040A0000|269 278|fibrinogen
-        // However, we ignore the offsets because the eval offsets ignore white spaces
-        List<Gene> geneList = new ArrayList<>(JCasUtil.select(jcas, Gene.class));
-        assertEquals("fibrinogen", geneList.get(0).getCoveredText());
-        assertEquals("fibrin", geneList.get(1).getCoveredText());
-        assertEquals("FR-antigen", geneList.get(2).getCoveredText());
-        assertEquals("cryoglobulin", geneList.get(3).getCoveredText());
-        assertEquals("fibrinogen", geneList.get(4).getCoveredText());
+		// expected result from the GENE.eval.small file:
+		// P00055040A0000|116 125|fibrinogen
+		// P00055040A0000|127 132|fibrin
+		// P00055040A0000|158 167|FR-antigen
+		// P00055040A0000|243 254|cryoglobulin
+		// P00055040A0000|269 278|fibrinogen
+		// However, we ignore the offsets because the eval offsets ignore white spaces
+		List<Gene> geneList = new ArrayList<>(JCasUtil.select(jcas, Gene.class));
+		assertEquals("fibrinogen", geneList.get(0).getCoveredText());
+		assertEquals("fibrin", geneList.get(1).getCoveredText());
+		assertEquals("FR-antigen", geneList.get(2).getCoveredText());
+		assertEquals("cryoglobulin", geneList.get(3).getCoveredText());
+		assertEquals("fibrinogen", geneList.get(4).getCoveredText());
 
-    }
+	}
 
 }
